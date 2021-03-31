bb0d57eaf5eb70649ecdb55b085a4bf1f4bde540
TheJis <62614133+ingTheJis@users.noreply.github.com>
2021-03-30 19:50:05 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type task struct{
+	ID int  `json:id`
+	Name string `json:name`
+	Content string `json:content`
+}
+
+type allTasks []task
+
+var tasks = allTasks {
+	{
+		ID: 1,
+		Name: "Task 1",
+		Content: "some content",		
+	},
+}
+
+func indexRoute(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "welcome to my API")
+
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request){
+	var newTask task
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil{
+		fmt.Fprintf(w, "Insert a valid task")
+	}
+	json.Unmarshal(reqBody, &newTask)
+
+	newTask.ID = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil{
+		fmt.Fprintf(w, "ID invalid")
+		return
+	}
+
+	for _, task := range tasks{
+		if task.ID == taskID{
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(task)
+		}
+	}
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil{
+		fmt.Fprintf(w, "ID invalid")
+		return
+	}
+
+	for i, task := range tasks{
+		if task.ID == taskID{
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			fmt.Fprintf(w, "task %v has been delete succesfully", taskID)
+		}
+	}
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	var updatedTask task
+
+	taskID, err := strconv.Atoi(vars["id"])
+	if err != nil{
+		fmt.Fprintf(w, "ID invalid")
+	}	
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil{
+		fmt.Fprintf(w, "please enter valid data")
+	}
+	json.Unmarshal(reqBody, &updatedTask)
+
+	for i, task := range tasks{
+		if task.ID == taskID{
+			tasks = append(tasks[:1], tasks[i+1:]...)
+			updatedTask.ID = taskID
+			tasks = append(tasks, updatedTask)
+
+			fmt.Fprintf(w, "task with ID %v has been updated succesfully", taskID)
+		}
+	}
+}
+
+func main(){
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
